d884255f58e01eafe3cedf29eebd335234552fe6
Carlos Duarte <carlosduarte.1@hotmail.com>
2024-01-23 15:07:58 -0700

    first

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>Ahorros -> $ {{ ahorros }}</h1>
+  <button @click="deposita(100)">Depositar $100</button><br /><br />
+  <button @click="retira(100)" :disabled="deshabilitaBoton">Retirar $100</button>
+
+  <div>
+    <h4>Movimientos:</h4>
+    <ul>
+      <li v-for="(movimiento, index) in reverseArray" :key="index">
+        <span v-if="movimiento.monto > 0">Depósito</span>
+        <span v-else>Retiro</span>
+        | Monto: {{ movimiento.monto }} | Saldo: {{ movimiento.saldoActual }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script setup lang="ts">
+import { computed, ref } from "vue";
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+interface IMovimiento {
+  monto: number;
+  saldoActual: number;
+}
+
+const ahorros = ref<number>(0);
+const movimientos = ref<IMovimiento[]>([]);
+
+const updateMovimientos = (cantidad: number) => {
+  movimientos.value = [...movimientos.value, {
+    monto: cantidad,
+    saldoActual: ahorros.value
+  }];
+}
+
+const deposita = (cantidad: number) => {
+  ahorros.value += cantidad;
+  updateMovimientos(+cantidad);
+}
+
+const retira = (cantidad: number) => {
+  ahorros.value -= cantidad;
+  updateMovimientos(-cantidad);
+}
+
+const deshabilitaBoton = computed(() => {
+  return ahorros.value <= 0;
+});
+
+const reverseArray = computed(() => {
+  return [...movimientos.value].reverse();
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
